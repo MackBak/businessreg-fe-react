083aae225f4dff400653db4f60156d3ae7793616
Mack Bakkum <154362357+MackBak@users.noreply.github.com>
2025-02-02 19:48:20 +0100

    Some scss + home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Home from "./components/Home/Home";
-import './App.css';
+import './App.scss';
 
 function App() {
     return (
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 
 function Home() {
     return (
         <>
         <h1>Home Page</h1>
-            <button onClick={() => console.log('Button 1 clicked')}>Button 1</button>
-            <button onClick={() => console.log('Button 2 clicked')}>Button 2</button>
-            <button onClick={() => console.log('Button 3 clicked')}>Button 3</button>
-            <button onClick={() => console.log('Button 4 clicked')}>Button 4</button>
+            <button onClick={() => console.log('## Register clicked ##')}>Register</button>
+            <button onClick={() => console.log('## Login clicked ##')}>Login</button>
+            <button onClick={() => console.log('## Register Business clicked ##')}>Register Business</button>
+            <button onClick={() => console.log('## Business Types clicked ##')}>Business Types</button>
         </>
     );
 }
